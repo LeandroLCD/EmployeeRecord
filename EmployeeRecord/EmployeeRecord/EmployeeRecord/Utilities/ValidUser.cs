a515516a0ea7bd869924f21d077458f6bba4a30f
LeandroLCD <clinicalsystem@outlook.com>
2023-03-07 02:19:18 -0300

    Login al 95%

---
+++ EmployeeRecord/EmployeeRecord/EmployeeRecord/Utilities/ValidUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace EmployeeRecord.Utilities
+{
+    public static class ValidUser
+    {
+        public static List<string> DataAnotationsValid(this object obj)
+        {
+            var context = new ValidationContext(obj, serviceProvider: null, items: null);
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(obj, context, results, true);
+
+            if (isValid)
+            {
+                return null;
+            }
+
+            var errores = results.Select(r => r.ErrorMessage).ToList();
+            return errores;
+        }
+    }
+}
