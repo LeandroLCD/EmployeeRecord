3c601f37b09931d42d36bdb5a23fb6579ce43fd0
LeandroLCD <clinicalsystem@outlook.com>
2023-03-09 22:20:49 -0300

    Add Email Service

--- EmployeeRecord/EmployeeRecord/EmployeeRecord/Service/Interface/IEmailService.cs
+++ EmployeeRecord/EmployeeRecord/EmployeeRecord/Service/Interface/IEmailService.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using EmployeeRecord.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EmployeeRecord.Service.Interface
 {
-    interface IEmailService
+    public interface IEmailService
     {
+        Task<response> SendEmail(string to, string subjet, string mensaje);
+
     }
 }
